1700b4ba12d4e25886ce521d1884133dca59835c
mtiessler <max.tiessler@upc.edu>
2024-12-16 22:14:04 +0100

    correct titles

--- UE/Assignment_2/src/components/Menu.tsx
+++ UE/Assignment_2/src/components/Menu.tsx
@@ -5,19 +5,19 @@ import '../styles/menu.scss';
 const Menu: React.FC = () => {
     return (
         <div className="menu-container">
-            <h1 className="menu-title">Welcome to the App</h1>
+            <h1 className="menu-title">IV Assignment 2</h1>
             <div className="menu-buttons">
                 <Link to="/view1">
-                    <button className="menu-button view1">View 1</button>
+                    <button className="menu-button view1">Geographical View 1</button>
                 </Link>
                 <Link to="/view2">
-                    <button className="menu-button view2">View 2</button>
+                    <button className="menu-button view2">Geographical View 2</button>
                 </Link>
                 <Link to="/view3">
-                    <button className="menu-button view3">View 3</button>
+                    <button className="menu-button view3">Geographical View 3</button>
                 </Link>
                 <Link to="/view4">
-                    <button className="menu-button view4">View 4</button>
+                    <button className="menu-button view4">Graph View</button>
                 </Link>
             </div>
         </div>
