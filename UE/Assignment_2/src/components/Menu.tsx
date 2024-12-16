5c2ae1322c165e2a5d8c5270cc1f5750e7d17abd
mtiessler <max.tiessler@upc.edu>
2024-12-16 22:19:44 +0100

    fixed routing menu template

--- UE/Assignment_2/src/components/Menu.tsx
+++ UE/Assignment_2/src/components/Menu.tsx
@@ -7,16 +7,16 @@ const Menu: React.FC = () => {
         <div className="menu-container">
             <h1 className="menu-title">IV Assignment 2</h1>
             <div className="menu-buttons">
-                <Link to="/view1">
+                <Link to="/geo-1">
                     <button className="menu-button view1">Geographical View 1</button>
                 </Link>
-                <Link to="/view2">
+                <Link to="/geo-2">
                     <button className="menu-button view2">Geographical View 2</button>
                 </Link>
-                <Link to="/view3">
+                <Link to="/geo-3">
                     <button className="menu-button view3">Geographical View 3</button>
                 </Link>
-                <Link to="/view4">
+                <Link to="/graph">
                     <button className="menu-button view4">Graph View</button>
                 </Link>
             </div>
