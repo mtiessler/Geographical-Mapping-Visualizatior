c07f14b1791b7a7760abc2a4fbd7b144437d6ad3
Max Tiessler <max.tiessler@upc.edu>
2024-12-27 11:10:11 +0100

    updated data.json

--- UE/Assignment_2/src/components/GeoView1.tsx
+++ UE/Assignment_2/src/components/GeoView1.tsx
@@ -36,11 +36,11 @@ const GeoView1 = () => {
     const fetchData = async () => {
         try {
             const response = await fetch('/data/data.json'); // Ensure this path is correct
-            if (!response.ok) throw new Error('Failed to fetch data.json');
+            if (!response.ok) throw new Error('Failed to fetch 1_geographic_timeline.json');
             const exhibitionData = await response.json();
             return exhibitionData;
         } catch (error) {
-            console.error('Error fetching data.json:', error);
+            console.error('Error fetching 1_geographic_timeline.json:', error);
             return null;
         }
     };
