3392156e0abfce46530be0ff267b9920f52caa78
mtiessler <max.tiessler@upc.edu>
2025-01-12 00:41:24 +0100

    new aesthetic

--- UE/Assignment_2/src/components/GeoView2.tsx
+++ UE/Assignment_2/src/components/GeoView2.tsx
@@ -103,7 +103,7 @@ const GeoView2 = () => {
 
         const tooltip = d3.select(tooltipRef.current);
 
-        const width = 900;
+        const width = 700; // Adjusted width for better layout
         const height = 500;
 
         const geoJSON = topojson.feature(
@@ -119,6 +119,15 @@ const GeoView2 = () => {
         const colorScale = d3
             .scaleSequentialLog(d3.interpolateReds)
             .domain([1, maxExhibitionsOverall]);
+
+        const zoom = d3.zoom<SVGSVGElement, unknown>()
+            .scaleExtent([1, 8])
+            .translateExtent([[0, 0], [width, height]])
+            .on('zoom', (event) => {
+                svg.select('g').attr('transform', event.transform);
+            });
+
+        svg.call(zoom);
 
         const g = svg.append('g');
 
@@ -216,29 +225,65 @@ const GeoView2 = () => {
     }, [worldMap, data, selectedArtist]);
 
     return (
-        <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div
+            style={{
+                padding: '20px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                minHeight: '100vh',
+                background: 'linear-gradient(to top, #0054a4, #ffffff)',
+                color: '#0054a4',
+                fontFamily: 'Arial, sans-serif',
+                overflowY: 'auto',
+            }}
+        >
             <button
                 style={{
                     alignSelf: 'flex-start',
-                    marginBottom: '10px',
+                    marginBottom: '20px',
                     padding: '10px 20px',
                     fontSize: '14px',
+                    fontWeight: 'bold',
+                    color: '#0054a4',
+                    backgroundColor: '#ffffff',
+                    border: '2px solid #0054a4',
+                    borderRadius: '5px',
                     cursor: 'pointer',
+                    transition: 'all 0.3s ease',
                 }}
                 onClick={() => (window.location.href = '/')}
+                onMouseEnter={(e) => {
+                    e.currentTarget.style.backgroundColor = '#0054a4';
+                    e.currentTarget.style.color = '#ffffff';
+                }}
+                onMouseLeave={(e) => {
+                    e.currentTarget.style.backgroundColor = '#ffffff';
+                    e.currentTarget.style.color = '#0054a4';
+                }}
             >
                 Go Back
             </button>
-            <h1>Geographical Heatmap of Exhibitions</h1>
-            <p style={{ maxWidth: '600px', textAlign: 'center', margin: '10px 0' }}>
-                This map visualizes the number of exhibitions held in various countries by artist. You are
-                currently viewing data for <strong>{selectedArtist}</strong>. Use the dropdown below to
-                select a different artist.
+
+            <h1 style={{ color: '#0054a4' }}>Geographical Heatmap of Exhibitions</h1>
+            <p style={{ maxWidth: '600px', textAlign: 'center', margin: '10px 0', color: '#0054a4' }}>
+                This map visualizes the number of exhibitions held in various countries by artist.
+                You are currently viewing data for <strong>{selectedArtist}</strong>.
+                Use the dropdown below to select a different artist.
             </p>
+
             <select
                 value={selectedArtist}
                 onChange={(e) => setSelectedArtist(e.target.value)}
-                style={{ marginBottom: '20px', padding: '10px', fontSize: '14px' }}
+                style={{
+                    marginBottom: '20px',
+                    padding: '10px',
+                    fontSize: '14px',
+                    border: '2px solid #0054a4',
+                    borderRadius: '5px',
+                    color: '#0054a4',
+                    backgroundColor: '#ffffff',
+                }}
             >
                 {artists.map((artist) => (
                     <option key={artist} value={artist}>
@@ -246,45 +291,126 @@ const GeoView2 = () => {
                     </option>
                 ))}
             </select>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <svg ref={svgRef} width={900} height={500} style={{ border: '1px solid #ccc' }}></svg>
-                <svg ref={legendRef} width={100} height={350} style={{ marginLeft: '10px' }}></svg>
-            </div>
+
             <div
-                ref={tooltipRef}
                 style={{
-                    position: 'absolute',
-                    visibility: 'hidden',
-                    background: '#fff',
-                    padding: '5px',
-                    border: '1px solid #ccc',
-                    borderRadius: '4px',
-                    pointerEvents: 'none',
-                    zIndex: 10,
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#ffffff',
+                    padding: '10px',
+                    borderRadius: '10px',
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                    width: '100%',
+                    maxWidth: '700px',
                 }}
-            ></div>
-            <table style={{ marginTop: '20px', borderCollapse: 'collapse', width: '80%', textAlign: 'left' }}>
-                <thead>
-                <tr style={{ backgroundColor: '#f2f2f2' }}>
-                    <th style={{ padding: '10px', border: '1px solid #ddd' }}>Country</th>
-                    <th style={{ padding: '10px', border: '1px solid #ddd' }}>Exhibitions</th>
-                </tr>
-                </thead>
-                <tbody>
-                {filteredData().map((entry, index) => (
-                    <tr key={index}>
-                        <td style={{ padding: '10px', border: '1px solid #ddd' }}>{entry['e.country']}</td>
-                        <td style={{ padding: '10px', border: '1px solid #ddd' }}>{entry.num_exhibitions}</td>
+            >
+                <svg
+                    ref={svgRef}
+                    width="100%"
+                    height="500px"
+                    style={{ border: '1px solid #ccc', maxWidth: '700px' }}
+                ></svg>
+                <svg
+                    ref={legendRef}
+                    width={100}
+                    height={350}
+                    style={{ marginLeft: '10px' }}
+                ></svg>
+            </div>
+
+            <div
+                style={{
+                    marginTop: '20px',
+                    width: '90%',
+                    maxHeight: '400px',
+                    overflow: 'auto',
+                    border: '1px solid #ccc',
+                    borderRadius: '8px',
+                    backgroundColor: '#ffffff',
+                }}
+            >
+                <table
+                    style={{
+                        width: '100%',
+                        borderCollapse: 'collapse',
+                        textAlign: 'left',
+                        color: '#0054a4',
+                    }}
+                >
+                    <thead>
+                    <tr style={{ backgroundColor: '#f2f2f2', color: '#0054a4' }}>
+                        <th
+                            style={{
+                                padding: '10px',
+                                border: '1px solid #ddd',
+                                position: 'sticky',
+                                top: 0,
+                                backgroundColor: '#f2f2f2',
+                                zIndex: 1,
+                            }}
+                        >
+                            Country
+                        </th>
+                        <th
+                            style={{
+                                padding: '10px',
+                                border: '1px solid #ddd',
+                                position: 'sticky',
+                                top: 0,
+                                backgroundColor: '#f2f2f2',
+                                zIndex: 1,
+                            }}
+                        >
+                            Exhibitions
+                        </th>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {filteredData().map((entry, index) => (
+                        <tr key={index}>
+                            <td
+                                style={{
+                                    padding: '10px',
+                                    border: '1px solid #ddd',
+                                }}
+                            >
+                                {entry['e.country']}
+                            </td>
+                            <td
+                                style={{
+                                    padding: '10px',
+                                    border: '1px solid #ddd',
+                                }}
+                            >
+                                {entry.num_exhibitions}
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
             <button
                 style={{
                     marginTop: '20px',
                     padding: '10px 20px',
                     fontSize: '14px',
+                    fontWeight: 'bold',
+                    color: '#0054a4',
+                    backgroundColor: '#ffffff',
+                    border: '2px solid #0054a4',
+                    borderRadius: '5px',
                     cursor: 'pointer',
+                    transition: 'all 0.3s ease',
+                }}
+                onMouseEnter={(e) => {
+                    e.currentTarget.style.backgroundColor = '#0054a4';
+                    e.currentTarget.style.color = '#ffffff';
+                }}
+                onMouseLeave={(e) => {
+                    e.currentTarget.style.backgroundColor = '#ffffff';
+                    e.currentTarget.style.color = '#0054a4';
                 }}
                 onClick={exportTableData}
             >
